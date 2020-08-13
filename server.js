c3dd7add41b5eb04a95a0e3d31c9d4653baac4da
Rob Ortiz <rob@dragonwrench.com>
2020-08-12 23:19:43 -0500

    initial checkin

---
+++ server.js
@@ -0,0 +1,52 @@
+let app = require('express')();
+let http = require('http').Server(app);
+let io = require('socket.io')(http);
+let bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+});
+
+
+http.listen(3000, () => {
+  console.log('Listening on port *: 3000');
+});
+
+io.on('connection', (socket) => {
+
+  app.post('/', function(req, res) {
+    console.log(req.body);
+    socket.broadcast.emit('chat-message', {
+      username: req.body.agent,
+      message: req.body.message
+    });
+    res.send(req.body);
+  });
+
+  socket.emit('connections', Object.keys(io.sockets.connected).length);
+
+  socket.on('disconnect', () => {
+    console.log("A user disconnected");
+  });
+
+  socket.on('chat-message', (data) => {
+    socket.broadcast.emit('chat-message', (data));
+  });
+
+  socket.on('typing', (data) => {
+    socket.broadcast.emit('typing', (data));
+  });
+
+  socket.on('stopTyping', () => {
+    socket.broadcast.emit('stopTyping');
+  });
+
+  socket.on('joined', (data) => {
+    socket.broadcast.emit('joined', (data));
+  });
+
+  socket.on('leave', (data) => {
+    socket.broadcast.emit('leave', (data));
+  });
+});
